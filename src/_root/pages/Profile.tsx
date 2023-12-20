c2909b37cbfc00562823885cd39ac994a93f141a
Potism <122753856+Potism@users.noreply.github.com>
2023-12-20 20:51:18 +0100

    fix Post profile

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -63,7 +63,7 @@ const Profile = () => {
             <div className="flex gap-8 mt-10 items-center justify-center xl:justify-start flex-wrap z-20">
               <StatBlock
                 value={currentUser.post ? currentUser.post.length : 0}
-                label="post"
+                label="Post"
               />
 
               <StatBlock
